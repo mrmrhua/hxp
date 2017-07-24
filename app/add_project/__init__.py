a7063a3e5b5d3b99f8a2edfdd249a9ffb8facae6
DingYH <mrmrhua@126.com>
2017-07-24 14:45:57 +0800

    网站升级＋设计师入驻实现

--- app/add_project/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api,Resource
-
-add_project = Blueprint('add_project',__name__)
-
-from . import views
--- app/designers/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api,Resource
-
-designers = Blueprint('designers',__name__)
-
-from . import views
